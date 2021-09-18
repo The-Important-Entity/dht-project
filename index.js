04d618e0ec6ff4d7667cc5a288cd7fee964f8257
unknown <joseph.scalera1@gmail.com>
2021-09-18 14:55:39 -0400

    minor update

--- index.js
+++ index.js
@@ -16,15 +16,3 @@ class DHT_Node {
 }
 
 module.exports = DHT_Node;
-
-// const TABLE_SIZE = process.env.TABLESIZE || 1000000;
-
-// const args = process.argv.slice(2)
-// const port = args[0];
-// const url = args[1] + ":" + args[0];
-// const id = args[2]
-// const id_max = process.env.ID_MAX;
-// const data_dir = process.env.DATA_DIR;
-
-// var dht = new DHT_Node(config(port, url, id, id_max, data_dir));
-// dht.listen();
